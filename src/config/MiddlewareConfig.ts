b93d105b5885a589f0b355473814a01c5d079fec
Jason S. Jones <jsjones96@gmail.com>
2019-02-04 20:08:00 -0800

    chore: add body-parser middleware

--- src/config/MiddlewareConfig.ts
+++ src/config/MiddlewareConfig.ts
@@ -1,3 +1,4 @@
+import bodyParser from 'body-parser';
 import { Application } from 'express';
 import morgan from 'morgan';
 
@@ -5,6 +6,8 @@ import config from './config';
 
 class MiddlewareConfig {
     public static configMiddleware(app: Application) {
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({ extended: false }));
         if (config.env === 'development') {
             app.use(morgan('dev'));
         }
